a3c7f8ae85cb04cbff9d4a19ae351be9930cb058
Bhavesh Kalisetti <bhaveshkalisetti@gmail.com>
2026-01-06 23:53:11 -0800

    use Pareto Distribution for Level 1 Problem 100
    With inputs sampled from Unif(0,1), we can directly compute the expected value of the output using the mean of the targets.
    
    We use the Pareto distribution to sample inputs w/finite mean and infinite variance to prevent hacking this way.

--- KernelBench/level1/100_HingeLoss.py
+++ KernelBench/level1/100_HingeLoss.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from torch.distributions import Pareto
+
 class Model(nn.Module):
     """
     A model that computes Hinge Loss for binary classification tasks.
@@ -19,7 +21,9 @@ input_shape = (32768,)
 dim = 1
 
 def get_inputs():
-    return [torch.rand(batch_size, *input_shape), torch.randint(0, 2, (batch_size,)).float() * 2 - 1]
+    predictions = Pareto(0.01, 1.5).sample((batch_size, *input_shape))
+    targets = torch.randint(0, 2, (batch_size,)).float() * 2 - 1
+    return [predictions, targets]
 
 def get_init_inputs():
     return []
